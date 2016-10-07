c95a179e63fc68c8d0fc54a61b97acfc95d2279e
Audrey Carlsen <acarlsen@seattletimes.com>
2016-10-07 16:05:09 -0700

    alternate logo

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,24 @@ var Reveal = require("./reveal");
 Reveal.initialize({
   mouseWheel: true
 });
+
+var whitelisted = {
+  2: true,
+  4: true,
+  6: true,
+  8: true,
+  9: true,
+  10: true,
+  11: true,
+  13: true,
+  15: true,
+  16: true
+}
+
+Reveal.addEventListener("slidechanged", function(event) {
+  if (event.indexv in whitelisted) {
+    document.body.classList.add("black-logo");
+  } else {
+    document.body.classList.remove("black-logo");
+  }
+});
