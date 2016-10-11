c9a5515804ab4b129f9bae6af37bb7434078f1f3
Audrey Carlsen <acarlsen@seattletimes.com>
2016-10-10 17:11:12 -0700

    videos auto play

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
 require("./lib/social");
+var async = require("async");
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
 var Reveal = require("./reveal");
+var ready = require("./brightcove");
 
 Reveal.initialize({
-  mouseWheel: true
+  width: 1020,
+  mouseWheel: true,
+  slideNumber: true,
+  history: true
 });
 
 var whitelisted = {
@@ -26,4 +31,32 @@ Reveal.addEventListener("slidechanged", function(event) {
   } else {
     document.body.classList.remove("black-logo");
   }
+});
+
+// Load video player
+var players = {
+  6: "player-1",
+  12: "player-2",
+  16: "player-3",
+  26: "player-4"
+}
+
+async.mapValues(players, function(val, key, callback) {
+  ready("Nk8AFQkhe", val, p => callback(null, p));
+}, function(err, players) {
+  //called when all players are initialized
+  console.log(players)
+  Reveal.addEventListener("slidechanged", function(event) {
+    if (event.indexv in players) {
+      players[event.indexv].play();
+    } else {
+      players[event.indexv].stop();
+    }
+  });
+})
+
+ready("Nk8AFQkhe", "player-1", function(p) {
+  // p.on("ended", function() {
+  //   animateScroll("#words");
+  // })
 });
